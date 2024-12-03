2344d57fc77152102dec1fc81d28888703b2f248
Barkhasbadi Ankhbileg <barkhasbadi.a@mbank.local>
2024-12-03 10:20:39 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Person from "./components/Person";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [newName, setNewName] = useState("...");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <Person key={person.id} person={person} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Person.jsx
@@ -0,0 +1,6 @@
+const Person = ({ person }) => {
+    return <li key={person.id}>{person.name}</li>;
+  };
+  
+  export default Person;
+  
